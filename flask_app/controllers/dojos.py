7dedf3d3c1a6d5708649e46b8a2e1c0f512bbc68
Violetta Szewczyk <szewczykviolet@gmail.com>
2021-11-06 18:33:25 -0500

    style changes

--- flask_app/controllers/dojos.py
+++ flask_app/controllers/dojos.py
@@ -1,6 +1,7 @@
 from flask_app import app
 from flask import render_template,redirect,request
 from flask_app.models.dojo import Dojo
+from flask_app.models.ninja import Ninja
 
 @app.route("/")
 def index():
@@ -23,8 +24,20 @@ def dojo_info(id):
     dojo_ninjas = Dojo.get_dojo_ninjas(data)
     return render_template("show_dojo.html",dojo_ninjas=dojo_ninjas)
 
+    
+@app.route("/new")
+def add_ninja():
+    dojos_ninjas = Dojo.get_all_dojos()
+    return render_template("new_ninja.html",dojos_ninjas=dojos_ninjas)
+
 @app.route("/ninjas",methods=["POST"])
 def new_ninja():
     data = {
-        
+        "first_name":request.form["first_name"],
+        "last_name":request.form["last_name"],
+        "age":request.form["age"],
+        "dojo_id":request.form["dojo_id"]
     }
+    Ninja.create_ninja(data)
+    new_id = request.form["dojo_id"]
+    return redirect(f"/dojo/{new_id}")
--- flask_app/models/ninja.py
+++ flask_app/models/ninja.py
@@ -1,3 +1,5 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
 class Ninja:
     def __init__(self,data):
         self.id = data["id"]
@@ -6,3 +8,8 @@ class Ninja:
         self.age = data["age"]
         self.created_at = data["created_at"]
         self.updated_at = data["updated_at"]
+
+    @classmethod
+    def create_ninja(cls,data):
+        query = "INSERT INTO ninjas (first_name, last_name, age, created_at, updated_at, dojo_id) VALUES (%(first_name)s, %(last_name)s, %(age)s, NOW(), NOW(), %(dojo_id)s)"
+        return connectToMySQL("dojos_and_ninjas").query_db(query,data)
